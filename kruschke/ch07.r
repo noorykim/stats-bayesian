570d0cba67cb7da3fea8b3726aa1a4e73fc65795
Kim <NKim@Syncorp.local>
2018-01-25 16:26:25 -0500

    Added programs by Kruschke

--- kruschke/ch07.r
+++ kruschke/ch07.r
@@ -1,5 +1,9 @@
 # Kruschke, chapter 7
 
+install.packages("BRugs")
+library(BRugs)
+
 # 7.1 
 
-BernMetropolisTemplate.R
+BernMetropolisTemplate.R 
+
---
+++ kruschke/e2ch07.r
@@ -0,0 +1,9 @@
+# Kruschke 2e, chapter 7
+
+# path where copies of the author's programs are saved
+setwd("H:/Documents/stats/kruschke/ProgramsDoingBayesianDataAnalysis")
+
+# 7.1 
+
+BernMetropolisTemplate.R 
+
